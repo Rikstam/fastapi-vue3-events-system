4e121891ab06cd3b44a6bf422ea968e0aff3fb3b
Riku <rikukestila@gmail.com>
2022-09-20 10:27:51 +0300

    Add a basic JWT auth flow

---
+++ backend/app/api/users/auth.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from jose import jwt
+
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def hash_password(password: str) -> str:
+    """Hash a password string with bcrypt"""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    """Verify the password"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """Create a JWT access token"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
--- backend/app/api/users/users.py
+++ backend/app/api/users/users.py
@@ -1,26 +1,113 @@
-from fastapi import APIRouter, HTTPException, Path
+import os
+from datetime import timedelta
 
-from app.api.users import crud
+from jose import JWTError
+from fastapi import APIRouter,Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from typing import Union
 
 from app.models.pydantic import( 
     UserPayLoadSchema,
-    UserResponseSchema
+    UserResponseSchema,
+    Token,
+    TokenData,
 )
-
-from app.models.tortoise import UserSchema
-
+from app.models.tortoise import User
+from app.api.users import auth
 router = APIRouter()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_user(username: str) -> Union[dict, None]:
+    """Get a user by username"""
+    user = await User.filter(username=username).first()
+    if user:
+        return user
+    return None
+
+async def authenticate_user(username: str, password: str):
+    """Authenticate a user via username and password"""
+    user = await get_user(username)
+    if not user:
+        return False
+    if not auth.verify_password(password, user.hashed_password):
+        return False
+    return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    """After receiving a token, try to return a corresponding user"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = auth.decode_token(token)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 @router.post("/", response_model=UserResponseSchema, status_code=201)
 async def create_user(payload: UserPayLoadSchema) -> UserResponseSchema:
-    user_uuid = await crud.post(payload)
-
+    user = User(
+       email=payload.email,
+       username=payload.username,
+       first_name=payload.first_name,
+       last_name=payload.first_name,
+       hashed_password=auth.hash_password(payload.password)
+    )
+    await user.save()
+  
     response_object = {
-        "uuid": user_uuid,
+        "uuid": user.uuid,
         "email": payload.email,
         "username": payload.username,
         "first_name": payload.first_name,
         "last_name": payload.last_name,
-       
     }
     return response_object
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    """After authentication, obtain the JWT token"""
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = auth.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.get("/users/me/", response_model=UserResponseSchema)
+async def read_users_me(current_user: UserResponseSchema = Depends(get_current_active_user)):
+    return current_user
+
+@router.get("/{uuid}", response_model=UserResponseSchema)
+async def get_user_by_uuid(uuid: str):
+    user = await User.filter(uuid=uuid).first()
+    if user:
+        return user
+    return None
+
+@router.get("/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
